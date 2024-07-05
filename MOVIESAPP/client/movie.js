55466455e1474357b54e3b8eccb4bb18b747a64e
Jorge Chiozzini <jchiozzini@gmail.com>
2024-07-05 11:03:56 +0000

    add APIKEY add APILINK

--- MOVIESAPP/client/movie.js
+++ MOVIESAPP/client/movie.js
@@ -2,7 +2,7 @@ const url = new URL(location.href);
 const movieId = url.searchParams.get("id")
 const movieTitle = url.searchParams.get("title")
 
-const APILINK = '';
+const APILINK = '    /api/v1/reviews/';
 
 
 const main = document.getElementById("section");
--- MOVIESAPP/client/script.js
+++ MOVIESAPP/client/script.js
@@ -1,6 +1,6 @@
-const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=MINHAAPIKEY&page=1';
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=key=41ee980e4b5f05f6693fda00eb7c4fd4&page=1';
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=MINHAAPIKEY&query=";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=key=41ee980e4b5f05f6693fda00eb7c4fd4&query=";
 
 
 const main = document.getElementById("section");
